eb0bc45edbde045a57a2747f0449a5ca9e7a916e
Glitch (glitch-hello-react) <none>
2024-08-05 20:49:27 +0000

    ⛰🍡 Checkpoint ./src/App.jsx:59091006/5275

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SegmentDisplay from "./components/SegmentDisplay";
 import ModeSelector from "./components/ModeSelector";
 import InputForm from "./components/InputForm";
 import VideoScanner from "./components/VideoScanner";
-import { Block } from "./encode/Block";
+import { createBlocks } from "./encode/Block";
 import { getEncoder } from "./encode/Encoder";
 import { TaggedBitstream } from "./encode/TaggedBitstream";
 import { VERSIONS } from "./encode/version";
@@ -12,6 +12,7 @@ import { QRCodeMatrix } from "./qrCodeMatrix";
 
 const segments = [];
 const blocks = [];
+const dataCodewords = [];
 
 let bitStream;
 let errorCorrectionLevel;
@@ -34,18 +35,20 @@ function App() {
 
   const processQRCodeData = (chunks, version, formatInfo) => {
     const versionInfo = VERSIONS[version <= 6 ? version - 1 : version];
+    const { errorCorrectionLevels } = versionInfo;
 
     errorCorrectionLevel = formatInfo.errorCorrectionLevel;
-  bitStream = new TaggedBitstream();
-  for (const chunk of chunks) {
-    const { type, text, bytes, assignmentNumber } = chunk;
-    const data = text ? text : bytes ? bytes : assignmentNumber;
-    getEncoder({ type, bitStream }).encode(data);
-  }
-  blocks = createBlocks(bitStream, errorCorrectionLevel, versionInfo);
-
-    const newSegments = bitStream.segments;
-    setSegments(newSegments);
+    const { ecCodewordsPerBlock, ecBlocks } = errorCorrectionLevels[errorCorrectionLevel];
+    bitStream = new TaggedBitstream();
+    for (const chunk of chunks) {
+      const { type, text, bytes, assignmentNumber } = chunk;
+      const data = text ? text : bytes ? bytes : assignmentNumber;
+      getEncoder({ type, bitStream }).encode(data);
+    }
+    blocks = createBlocks(bitStream, errorCorrectionLevel, versionInfo);
+    
+    
+    setSegments(bitStream.segments);
 
     const qrMatrix = new QRCodeMatrix({ versionInfo, formatInfo });
     qrMatrix.placeFunctionPatterns();
@@ -84,18 +87,6 @@ function generateMatrixFromCodewords(codewords, versionInfo) {
   return qrMatrix.getMatrix();
 }
 
-function qrCodeGenerator({ chunks, formatInfo, versionInfo }) {
-  //console.log("QRCodeGenerator", { chunks, errorCorrectionLevel, version });
-  errorCorrectionLevel = formatInfo.errorCorrectionLevel;
-  bitStream = new TaggedBitstream();
-  for (const chunk of chunks) {
-    const { type, text, bytes, assignmentNumber } = chunk;
-    const data = text ? text : bytes ? bytes : assignmentNumber;
-    getEncoder({ type, bitStream }).encode(data);
-  }
-  blocks = createBlocks(bitStream, errorCorrectionLevel, versionInfo);
-}
-
 function generateErrorCorrection(blocks) {
   for (const block of blocks) {
     block.generateErrorCorrection();
@@ -103,7 +94,6 @@ function generateErrorCorrection(blocks) {
 }
 
 function getCodewords(blocks) {
-  generateErrorCorrection(blocks);
   let cw = [];
   let totalCodewords = blocks.reduce(
     (total, block) => total + block.totalCodewords,
@@ -117,38 +107,4 @@ function getCodewords(blocks) {
     cw.push(codeword);
   }
   return cw;
-}
-
-function createBlocks(bitStream, errorCorrectionLevel, version) {
-  console.log("createBlocks", { bitStream, errorCorrectionLevel, version });
-  const { errorCorrectionLevels } = version;
-  const { ecCodewordsPerBlock, ecBlocks } =
-    errorCorrectionLevels[errorCorrectionLevel];
-
-  let blocks = [];
-  let requiredDataCodewords = 0;
-
-  ecBlocks.forEach((group) => {
-    const { numBlocks, dataCodewordsPerBlock } = group;
-    requiredDataCodewords += numBlocks * dataCodewordsPerBlock;
-    for (let i = 0; i < numBlocks; i++) {
-      const block = new Block(dataCodewordsPerBlock, ecCodewordsPerBlock, i);
-      blocks.push(block);
-    }
-  });
-
-  // Complete bytes and add padding
-  bitStream.finalize(requiredDataCodewords);
-  // fill blocks with codewords
-  for (const block of blocks) {
-    const { dataCodewords, numDataCodewords } = block;
-    while (dataCodewords.length < numDataCodewords) {
-      const taggedBits = bitStream.readTaggedByte();
-      const codeword = new TaggedCodeword(taggedBits, dataCodewords.length);
-      dataCodewords.push(codeword);
-    }
-  }
-
-  //console.log({ blocks });
-  return blocks;
 }
