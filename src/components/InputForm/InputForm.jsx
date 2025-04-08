d6f20e4c6cb38efc1265148728391b6023546131
Glitch (glitch-hello-react) <none>
2025-04-08 16:41:56 +0000

    🏟📫 Checkpoint ./src/components/InputForm/InputForm.jsx:59091006/29

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -58,7 +58,7 @@ function InputForm({ inputs, setInputs, processQRCodeData }) {
           }
           bytes = new Uint8Array(bin.length / 8);
           for (let i = 0; i < bin.length; i += 8) {
-            bytes[i / 8] = parseInt(bin.substring(i, i + 8), 2);
+            bytes.concat(parseInt(bin.substring(i, i + 8), 2));
           }
         } else if (hexRE.test(newValue)) {
           let hex = newValue.replace(/0x/gi, "");
