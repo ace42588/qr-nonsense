878696fcc61f734789b0ba0c4e083c3929ccbf86
Glitch (glitch-hello-react) <none>
2025-04-08 23:58:03 +0000

    🦁💏 Checkpoint ./src/components/MerchForm/MerchForm.jsx:59091006/1767

--- src/components/MerchForm/MerchForm.jsx
+++ src/components/MerchForm/MerchForm.jsx
@@ -3,61 +3,22 @@ import "./MerchForm.css"; // Import your component-specific styles
 
 const modes = ["p1", "p2", "p3"]; // Available modes
 
+const jsonLike = (order) => {
+  
+}
+
 const parseInput = (input) => {
-  let { type, value, encoding } = input;
-  let parsedInput = { type };
+  const { type, value } = input;
+  let order = JSON.parse(value);
+  let parsedInput = {};
 
   switch (type) {
     case "p1": {
-      const regex = /[0-9A-Z \$\%\*\+\-\.\/:]+/gm;
-      let upperCase = value.toUpperCase();
-      const match = upperCase.match(regex);
-      parsedInput.text = match ? match.join("") : "";
+      parsedInput.type = "alphanumeric"
+      parsedInput.text = jsonLike(order);
       break;
     }
-    case "byte": {
-      const isBinary = (str) =>
-        /^(?:0b)?(?:[01]{8}(?:\s+[01]{8})+|(?:[01]{8})+)$/i.test(str);
-      const isHex = (str) =>
-        /^(?:0x)?(?:[0-9A-F]{2}(?:\s+[0-9A-F]{2})+|(?:[0-9A-F]{2})+)$/i.test(
-          str
-        );
-      const forceUtf = encoding === "utf-8";
-
-      if (isBinary(value) && !forceUtf) {
-        let hex = "";
-        let bin = value.replace(/^0b/i, "");
-        bin = bin.replace(/\s+/g, "");
-
-        if (bin.length % 8 !== 0) {
-          throw new Error(
-            "Invalid binary string: length must be byte aligned."
-          );
-        }
-
-        for (let i = 0; i < bin.length; i += 4) {
-          let val = parseInt(bin.substring(i, i + 4), 2);
-          hex = hex.concat(val.toString(16));
-        }
-        parsedInput.encoding = "hex";
-        parsedInput.bytes = hex;
-      } else if (isHex(value) && !forceUtf) {
-        let hex = value.replace(/0x/gi, "");
-        hex = hex.replace(/\s+/g, "");
-
-        if (hex.length % 2 !== 0) {
-          throw new Error("Invalid hex string: length must be even.");
-        }
-        parsedInput.encoding = "hex";
-        parsedInput.bytes = hex;
-      } else {
-        console.log(
-          "input value for byte mode did not match binary or hex encoding"
-        );
-        parsedInput.encoding = "utf-8";
-        parsedInput.text = value;
-      }
-
+    case "p2": {
       break;
     }
     default: {
