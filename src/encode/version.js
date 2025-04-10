7b052846efe4ed28d06d5f3b9cb9d7bb92b58904
Glitch (glitch-hello-react) <none>
2025-04-10 14:09:48 +0000

    👠🍳 Checkpoint ./src/encode/version.js:59091006/84893

--- src/encode/version.js
+++ src/encode/version.js
@@ -1,3 +1,4 @@
+import { ErrorCorrectionLevel } from "./FormatInfo";
 /**
  * Given an array of data chunks and an error correction level,
  * returns the smallest QR code version (1 to 40) that can hold the data.
