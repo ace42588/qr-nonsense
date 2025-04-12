cfe519dd3c5d47d0399d853d9d6c045c8137d6ed
Glitch (glitch-hello-react) <none>
2025-04-12 15:29:43 +0000

    🍂🌻 Checkpoint ./src/context/BitstreamContext.js:59091006/1874 ./src/context/BitstreamReducer.js:59091006/229

--- src/context/BitstreamContext.js
+++ src/context/BitstreamContext.js
@@ -1,6 +1,37 @@
-import React, { createContext, useReducer } from "react’;
-import BitstreamReducer from ‘./BitstreamReducer’
+import React, { createContext, useReducer } from "react";
+import BitstreamReducer from "./BitstreamReducer";
 
-export const BitstreamContextProvider = ({children}) => {
+const initialState = {
+  bits: [],
+};
+
+export const BitstreamContext = createContext(initialState);
+
+export const BitstreamContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(BitstreamReducer, initialState);
+
+  function addBit(bit) {
+    dispatch({
+      type: "ADD_BIT",
+      payload: bit,
+    });
+  }
   
-}
+  function addByte(byte) {
+    dispatch({
+      type: "ADD_BIT",
+      payload: byte,
+    });
+  }
+
+  return (
+    <BitstreamContext.Provider
+      value={{
+        bits: state.bits,
+        addBit,
+      }}
+    >
+      {children}
+    </BitstreamContext.Provider>
+  );
+};
