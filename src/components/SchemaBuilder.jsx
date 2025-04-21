b2145b4522472563f2f1b6654e3b8a85e41df5d3
Glitch (verbena-dear-paradox) <none>
2025-04-21 16:57:26 +0000

    🎄👚 Checkpoint ./src/components/SchemaBuilder.jsx:59091006/60

--- src/components/SchemaBuilder.jsx
+++ src/components/SchemaBuilder.jsx
@@ -110,6 +110,7 @@ function FieldEditor({ field, path, onChange, onAddChild }) {
 
 // Utility functions
 function insertAtPath(schema, path, field) {
+  console.debug("insertAtPath", {schema, path, field});
   const lastKey = path.pop();
   let obj = schema;
   for (const key of path) obj = obj[key];
