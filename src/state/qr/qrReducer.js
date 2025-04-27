9bf94442127d3569df90c6bd20bfe5b4652ebafd
Glitch (verbena-dear-paradox) <none>
2025-04-27 14:35:46 +0000

    😹🚵 Checkpoint ./src/state/qr/qrReducer.js:59091006/6810

--- src/state/qr/qrReducer.js
+++ src/state/qr/qrReducer.js
@@ -14,41 +14,36 @@ export const initialData = {
   matrix: null,
 };
 
-function getModulesToHighlight(segment, ) {
-  
-}
+function getModulesToHighlight(segment) {}
 
 function highlightModules(segment, matrix) {
   const modulesToUpdate = getModulesToHighlight(segment);
-        const newMatrix = matrix.map((row) =>
-          row.map((module) => {
-            let { bit, isHighlighted } = module;
-            const newModule = { ...module };
-            if (bit.id && modulesToUpdate.some(({ id }) => id === bit.id)) {
-              newModule.isHighlighted = !isHighlighted;
-            }
-            return newModule;
-          })
-        );
+  const newMatrix = matrix.map((row) =>
+    row.map((module) => {
+      let { bit, isHighlighted } = module;
+      const newModule = { ...module };
+      if (bit.id && modulesToUpdate.some(({ id }) => id === bit.id)) {
+        newModule.isHighlighted = !isHighlighted;
+      }
+      return newModule;
+    })
+  );
   return newMatrix;
 }
 
-function getSegmentToHighlight(module) {
-  
-}
+function getSegmentToHighlight(module) {}
 
 function highlightSegment(module, segments) {
   const { bit, nonData } = module;
 
-        const segmentToUpdate = getSegmentToHighlight(module);
-        if (!segmentToUpdate) return state;
-        const newSegments = state.segments.map((segment) => {
-          let { id, isHighlighted } = segment;
-          const newSegment = { ...segment };
-          if (id === segmentToUpdate.id)
-            newSegment.isHighlighted = !isHighlighted;
-          return newSegment;
-        });
+  const segmentToUpdate = getSegmentToHighlight(module);
+  if (!segmentToUpdate) return;
+  const newSegments = segments.map((segment) => {
+    let { id, isHighlighted } = segment;
+    const newSegment = { ...segment };
+    if (id === segmentToUpdate.id) newSegment.isHighlighted = !isHighlighted;
+    return newSegment;
+  });
 }
 
 export function dataReducer(state, action) {
