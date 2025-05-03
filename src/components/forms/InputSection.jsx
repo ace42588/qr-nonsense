818452c98170022a349f908b18524f0a5437d766
Glitch (verbena-dear-paradox) <none>
2025-05-03 17:32:18 +0000

    🚄🌧 Checkpoint ./src/components/forms/InputSection.jsx:59091006/2706

--- src/components/forms/InputSection.jsx
+++ src/components/forms/InputSection.jsx
@@ -4,12 +4,10 @@ import "../styles/styles.css";
 import { BasicInput, JsonInput } from "../inputs";
 import BitFieldSection from "../BitFieldEditor/BitFieldSection";
 
-export function InputSection() {
-  const [input, setInput] = useState();
-  const [method, setMethod] = useState();
+const types = ["basic", "json", "bitField"];
 
-  const handleMethodChange = () => {};
-
+export function InputSection({ onChange, onRemove }) {
+  const [type, setType] = useState();
   return (
     <div className="row">
       <div
@@ -19,7 +17,26 @@ export function InputSection() {
           padding: 16,
           maxWidth: 900,
         }}
-      ></div>
+      >
+        <label htmlFor="inputMode">Input Mode:</label>
+        <select
+          id="inputMode"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        >
+          {types.map((t, idx) => (
+            <option key={t} value={t}>
+              {t}
+            </option>
+          ))}
+        </select>
+        <button type="button" onClick={onRemove}>
+          ✖
+        </button>
+      </div>
+      {type === "basic" && (<BasicInput />)}
+      {type === "json" && (<JsonInput />)}
+      {type === "json" && (<JsonInput />)}
     </div>
   );
 }
