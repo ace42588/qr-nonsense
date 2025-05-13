09fc3796c9e73c34b408281ef589d98aa6152f74
Glitch (verbena-dear-paradox) <none>
2025-05-13 22:12:54 +0000

    🦃🍯 Checkpoint ./src/domain/index.js:59091006/107

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,2 +1,4 @@
 export { getCodewords, getEncodedMessage, getMatrix } from "./qr";
+export { getTypeExtensions } from "./input/typeDefaults";
 export { parseAll } from "./input";
+export { MAC_FUNCTIONS } from "./input/parsers";
