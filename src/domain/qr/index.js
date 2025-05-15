5443200c4e1fe324b0c0c50cf1cfe724cfdee822
Glitch (verbena-dear-paradox) <none>
2025-05-15 18:43:00 +0000

    ⏱🐯 Checkpoint ./src/domain/qr/versionUtils.js:59091006/2888 ./src/domain/qr/index.js:59091006/1868

--- src/domain/qr/index.js
+++ src/domain/qr/index.js
@@ -1,6 +1,6 @@
 import { generateCodewords } from "./codewords";
 import { encodeAll, finalizeEncoding } from "./encoders";
-import { getMinimumQRCodeVersion } from "./versionUtils";
+import { getMinimumQRCodeVersion, gerVersionInfo } from "./versionUtils";
 
 export { getMatrix } from "./matrix";
 export { evaluateQRCodeQuality } from "./evaluator.js";
@@ -8,11 +8,11 @@ export { evaluateQRCodeQuality } from "./evaluator.js";
 export function getEncodedMessage(inputs, sVersion, errorCorrectionLevel) {
   sVersion = parseInt(sVersion);
   const [encodedInputs, numDataBits] = encodeAll(inputs);
-  const [version, numDataCodewords] =
+  const { version, requiredDataCodewords } =
     sVersion === -1
       ? getMinimumQRCodeVersion(numDataBits, errorCorrectionLevel)
-      : sVersion;
-  const segments = finalizeEncoding(encodedInputs, numDataCodewords);
+      : gerVersionInfo(errorCorrectionLevel, sVersion);
+  const segments = finalizeEncoding(encodedInputs, requiredDataCodewords);
   console.debug("getEncodedMessage", { segments, version });
   return { segments, version };
 }
--- src/domain/qr/versionUtils.js
+++ src/domain/qr/versionUtils.js
@@ -2,24 +2,23 @@ import { EC_INFO } from "./constants";
 
 function getRequiredCodewords(ecBlocks) {
   return ecBlocks.reduce(
-        (total, { numBlocks, dataCodewordsPerBlock }) =>
-          total + numBlocks * dataCodewordsPerBlock,
-        0
-      );
-  
+    (total, { numBlocks, dataCodewordsPerBlock }) =>
+      total + numBlocks * dataCodewordsPerBlock,
+    0
+  );
 }
 
 export function getMinimumQRCodeVersion(totalDataBits, errorCorrectionLevel) {
   // Try each version until one is found that fits the data.
   for (let version = 1; version <= 40; version++) {
-    const { capacity, ecBlocks } = gerVersionInfo(
+    const versionInfo = gerVersionInfo(
       errorCorrectionLevel,
       version
     );
+    const { capacity } = versionInfo
 
     if (totalDataBits <= capacity) {
-      const requiredDataCodewords = getRequiredCodewords(ecBlocks);
-      return [version, requiredDataCodewords];
+      return versionInfo;
     }
   }
   throw new Error("Data too large to fit in a QR code version 40.");
@@ -34,5 +33,9 @@ export function gerVersionInfo(errorCorrectionLevel, version) {
   if (!versionInfo) {
     throw new Error("Invalid QR version: " + version);
   }
-  return versionInfo;
+  return {
+    version,
+    ...versionInfo,
+    requiredDataCodewords: getRequiredCodewords(versionInfo.ecBlocks),
+  };
 }
