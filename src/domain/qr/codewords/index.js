bf5af252ad2813a660fcadf06ed9987d2804b8d8
Glitch (verbena-dear-paradox) <none>
2025-05-15 22:57:18 +0000

    🍋🚋 Checkpoint ./src/domain/qr/codewords/index.js:59091006/2086

--- src/domain/qr/codewords/index.js
+++ src/domain/qr/codewords/index.js
@@ -3,28 +3,26 @@ import { gerVersionInfo } from "../versionUtils";
 import { getCodewordsFromSegments } from "./utils";
 
 export function generateCodewords(segments, version, errorCorrectionLevel) {
-
   //const encodedBits = getBitsFromSegments(segments);
   const dataCodewords = getCodewordsFromSegments(segments);
-  
+
   const { ecCodewordsPerBlock, ecBlocks, remainderBits } = gerVersionInfo(
     errorCorrectionLevel,
     version
   );
 
   const qrBlocks = getBlocks(dataCodewords, ecCodewordsPerBlock, ecBlocks);
-  
+
   const length = qrBlocks.reduce(
     (total, { codewords }) => total + codewords.length,
     0
   );
+  console.debug("generateCodewords", { length });
   return Array.from({ length }, (_, idx) => {
     const blockIdx = idx % qrBlocks.length;
     const cwIdx = Math.floor(idx / qrBlocks.length);
-    const { codewords: bCodewords } = qrBlocks[blockIdx];
-    if (cwIdx < bCodewords.length) {
-      return bCodewords[cwIdx];
-    }
+    const { codewords } = qrBlocks[blockIdx];
+    return codewords[cwIdx];
   });
 }
 
