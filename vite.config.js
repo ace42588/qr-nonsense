80a12ab3faa8e4dbd652900c40ad71f6af433b5b
Glitch (verbena-dear-paradox) <none>
2025-05-17 21:38:25 +0000

    🔦🛤 Checkpoint ./src/assets/styles/index.css:59091006/24 ./vite.config.js:59091006/1616 ./package.json:59091006/20

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     {
       name: "watch-logger",
       handleHotUpdate({ file }) {
@@ -12,6 +15,11 @@ export default defineConfig({
       },
     },
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: "build",
   },
