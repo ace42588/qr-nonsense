bc5df903ccb38fee85abc934d8ed67fa1d6e13dc
Glitch (verbena-dear-paradox) <none>
2025-05-17 23:10:21 +0000

    🏎🎏 Checkpoint ./src/app/App.jsx:59091006/4129

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   QRDisplayTabs,
   SegmentDisplay,
@@ -8,32 +11,58 @@ import {
 
 import { InputProvider, QRDataProvider } from "../state";
 
+import { QRCodeCanvas } from "../components/qr/QRCodeCanvas";
+import { InputForm } from "../components/InputForm";
+import { VideoScanner } from "../components/VideoScanner";
+
 import "../assets/styles/App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="p-6 space-y-6">
       <InputProvider>
-      <QRDataProvider>
-        <div className="row">
-          <div className="column">
-            <div className="row">
-              <MainViewTabs />
+        <QRDataProvider>
+          <Tabs defaultValue="info" className="w-full">
+        <TabsList>
+          <TabsTrigger value="input">Input</TabsTrigger>
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="input">
+          <InputForm />
+        </TabsContent>
+
+        <TabsContent value="actions">
+          <div className="p-4 space-x-2">
+            <Button onClick={handleHighlight}>Highlight Segment</Button>
+            <Button variant="outline" onClick={clearHighlights}>Clear Highlights</Button>
+          </div>
+        </TabsContent>
+      </Tabs>
+          <Card>
+            <CardContent className="p-4">
+              <QRCodeCanvas />
+            </CardContent>
+          </Card>
+          <div className="row">
+            <div className="column">
+              <div className="row">
+                <MainViewTabs />
+              </div>
+            </div>
+            <div className="column">
+              <div className="row">
+                <QRDisplayTabs />
+              </div>
             </div>
           </div>
-          <div className="column">
-            <div className="row">
-              <QRDisplayTabs />
-            </div>
+          <div className="row">
+            <SegmentDisplay />
           </div>
-        </div>
-        <div className="row">
-          <SegmentDisplay />
-        </div>
-        <div className="row">
-          <CodewordDisplay />
-        </div>
-      </QRDataProvider>
+          <div className="row">
+            <CodewordDisplay />
+          </div>
+        </QRDataProvider>
       </InputProvider>
     </div>
   );
