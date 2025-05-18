dc82f11b67863f9dfdc8434d63053cac604c6dda
Glitch (verbena-dear-paradox) <none>
2025-05-18 18:34:03 +0000

    🚵⛄️ Checkpoint ./src/components/input-sidebar.jsx:59091006/11410

--- src/components/input-sidebar.jsx
+++ src/components/input-sidebar.jsx
@@ -1,12 +1,12 @@
-import * as React from "react"
-import { GalleryVerticalEnd, Minus, Plus } from "lucide-react"
+import * as React from "react";
+import { GalleryVerticalEnd, Minus, Plus } from "lucide-react";
 
-import { SearchForm } from "@/components/search-form"
+import { SearchForm } from "@/components/search-form";
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "@/components/ui/collapsible"
+} from "@/components/ui/collapsible";
 import {
   Sidebar,
   SidebarContent,
@@ -19,13 +19,23 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 import { FormatInput } from "./FormatInput";
-import { useInputs, useInputDispatch } from "../state";
+import { useInputs, useInputDispatch, useDerivedQRData } from "../state";
 import { SortableInput } from "./inputs/SortableInput";
 
+const levels = [
+  { label: "Low (L) – 7% redundancy", value: 0 },
+  { label: "Medium (M) – 15% redundancy", value: 1 },
+  { label: "Quartile (Q) – 25% redundancy", value: 2 },
+  { label: "High (H) – 30% redundancy", value: 3 },
+];
+
 export function AppSidebar({ ...props }) {
+  const { errorCorrectionLevel, version, dataMask, inputs } = useInputs();
+  const { version: cVersion, dataMask: cDataMask } = useDerivedQRData();
+  const { setErrorCorrection, setVersion, setDataMask } = useInputDispatch();
   return (
     <Sidebar {...props}>
       <SidebarHeader>
@@ -49,36 +59,44 @@ export function AppSidebar({ ...props }) {
       <SidebarContent>
         <SidebarGroup>
           <SidebarMenu>
-                          <Collapsible
-                key="formatInfo"
-                defaultOpen={false}
-                className="group/collapsible"
-              >
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton>
-                      Format Info
-                      <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
-                      <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                    <CollapsibleContent>
-                      <SidebarMenuSub>
-                        {item.items.map((item) => (
-                          <SidebarMenuSubItem key={item.title}>
-                            <SidebarMenuSubButton
-                              asChild
-                              isActive={item.isActive}
-                            >
-                              <a href={item.url}>{item.title}</a>
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        ))}
-                      </SidebarMenuSub>
-                    </CollapsibleContent>
-                </SidebarMenuItem>
-              </Collapsible>
-            {data.navMain.map((item, index) => (
+            <Collapsible
+              key="formatInfo"
+              defaultOpen={false}
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton>
+                    Format Info
+                    <Plus className="ml-auto group-data-[state=open]/collapsible:hidden" />
+                    <Minus className="ml-auto group-data-[state=closed]/collapsible:hidden" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem key="Error Correction Level">
+                      <SidebarMenuSubButton asChild isActive={true}>
+                        <label htmlFor="ec-level">
+                          Error Correction Level:
+                        </label>
+                        <select
+                          id="ec-level"
+                          value={errorCorrectionLevel}
+                          onChange={(e) => setErrorCorrection(e.target.value)}
+                        >
+                          {levels.map((level) => (
+                            <option key={level.value} value={level.value}>
+                              {level.label}
+                            </option>
+                          ))}
+                        </select>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+            {inputs.map((item, index) => (
               <Collapsible
                 key={item.title}
                 defaultOpen={index === 1}
@@ -116,5 +134,5 @@ export function AppSidebar({ ...props }) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
