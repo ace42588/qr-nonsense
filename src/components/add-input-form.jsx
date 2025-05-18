e73d28bfeac55b86340b13e08b4db3742ac770b5
Glitch (verbena-dear-paradox) <none>
2025-05-18 20:04:03 +0000

    🖐🎡 Checkpoint ./src/components/add-input-form.jsx:59091006/10209

--- src/components/add-input-form.jsx
+++ src/components/add-input-form.jsx
@@ -1,43 +1,40 @@
-import { Plus } from "lucide-react"
+import { useState, useRef } from "react";
 
-import { Label } from "@/components/ui/label"
+import { Plus } from "lucide-react";
+
+import { Label } from "@/components/ui/label";
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarInput,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+
+import { useInputs, useInputDispatch } from "../state";
 
 export function AddInput({ ...props }) {
+  const { inputs } = useInputs();
+  const { addInput, reorderInputs } = useInputDispatch();
+  const nextLabel = useRef(inputs?.length || 0);
+  const [label, setLabel] = useState("");
   return (
-    <form {...props}>
-      <SidebarGroup className="py-0">
-        <SidebarGroupContent className="relative">
-          <Label htmlFor="input" className="sr-only">
-            Add
-          </Label>
-          <SidebarInput
-            id="inputLabel"
-            value={label}
-            onChange={(e) => setLabel(e.target.value)}
-            placeholder="Add an input..."
-            className="pl-8"
-          />
-          <input
-            value={label}
-            onChange={(e) => setLabel(e.target.value)}
-            placeholder="Input Label"
-            required
-          />
+    <SidebarGroup className="py-0">
+      <SidebarGroupContent className="relative">
+        <Label htmlFor="input" className="sr-only">
+          Add
+        </Label>
+        <SidebarInput
+          id="inputLabel"
+          value={label}
+          onChange={(e) => setLabel(e.target.value)}
+          placeholder="Add an input..."
+          className="pl-8"
+        />
 
-          <button
-            onClick={() => {
-              addInput(label !== "" ? label : `Input ${nextLabel.current++}`);
-              setLabel("");
-            }}
-          >
-          <Plus className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
-        </SidebarGroupContent>
-      </SidebarGroup>
-    </form>
-  )
+        <Plus className="absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none" onClick={() => {
+            addInput(label !== "" ? label : `Input ${nextLabel.current++}`);
+            setLabel("");
+          }}/>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
 }
