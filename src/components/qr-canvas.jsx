fad6d334c7cb269ff61f95502563ca2ce94985fc
Glitch (verbena-dear-paradox) <none>
2025-05-19 00:01:24 +0000

    👨⛲️ Checkpoint ./src/components/qr-canvas.jsx:59091006/3266 ./src/index.css:59091006/119

--- src/components/qr-canvas.jsx
+++ src/components/qr-canvas.jsx
@@ -146,82 +146,20 @@ export function QRCanvasCard() {
         </div>
       </CardHeader>
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
-        >
-          <AreaChart data={filteredData}>
-            <defs>
-              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={1.0}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-desktop)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-mobile)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs>
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="date"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              minTickGap={32}
-              tickFormatter={(value) => {
-                const date = new Date(value)
-                return date.toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                })
-              }}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={
-                <ChartTooltipContent
-                  labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                    })
-                  }}
-                  indicator="dot"
-                />
-              }
-            />
-            <Area
-              dataKey="mobile"
-              type="natural"
-              fill="url(#fillMobile)"
-              stroke="var(--color-mobile)"
-              stackId="a"
-            />
-            <Area
-              dataKey="desktop"
-              type="natural"
-              fill="url(#fillDesktop)"
-              stroke="var(--color-desktop)"
-              stackId="a"
-            />
-          </AreaChart>
-        </ChartContainer>
+        <div className="qr-code-canvas-container">
+      <div className="relative w-[420px] h-[420px]">
+
+        <canvas className="absolute top-0 left-0 block w-full h-full pointer-events-none border border-black" id="qrCode" ref={qrRef} width="420" height="420"></canvas>
+        <canvas
+          className="absolute top-0 left-0 block w-full h-full pointer-events-none border border-black"
+          id="canvas"
+          ref={highlightRef}
+          width="420"
+          height="420"
+          style={{ border: "1px solid #000" }}
+        ></canvas>
+      </div>
+    </div>
       </CardContent>
     </Card>
   )
